72adc7edb8fb0ed263d822b842dbc1def8ad3c6f
Menno Finlay-Smits <inbox@menno.io>
2017-10-16 15:30:17 +1300

    Standalone tool for CPTV to MP4 conversion
    This is useful when experimenting with files locally.

--- cptvtool.py
+++ cptvtool.py
@@ -1,12 +1,14 @@
 #!/usr/bin/python3
 
 import sys
+import pytz
+
 from cptv import CPTVReader
 
-reader = CPTVReader(open(sys.argv[1], "rb"))
-print(reader.timestamp)
+local_tz = pytz.timezone('Pacific/Auckland')
 
-count = 0
-for frame in reader:
-    count += 1
-print(count)
+reader = CPTVReader(open(sys.argv[1], "rb"))
+print(reader.timestamp.astimezone(local_tz))
+
+for i, (frame, offset) in enumerate(reader):
+    print(i, offset, frame.min(), frame.max())
