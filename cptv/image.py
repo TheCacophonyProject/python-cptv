275b78689ccf45622ea5d12880b24cfc460b128a
Menno Finlay-Smits <inbox@menno.io>
2017-10-27 17:33:20 +1300

    Add LICENSE file and license headers
    Apache License 2.0
    
    Fixes #1.

--- cptv/image.py
+++ cptv/image.py
@@ -1,3 +1,17 @@
+# Copyright 2017 The Cacophony Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 from PIL import Image
 import cv2
 import numpy as np
@@ -6,6 +20,7 @@ import numpy as np
 def save_frame(frame, filename):
     rgb = process_frame_to_rgb(frame)
     rgb_to_png(rgb, filename)
+
 
 def process_frame_to_rgb(frame):
     a = np.zeros((120, 160))
@@ -22,6 +37,7 @@ def process_frame_to_rgb(frame):
     rgb[..., 1] = (b2*255 + b3*255 + b1*255*a*4/65535.0 + b4*255*((65535.0-a)*4/65535.0))
     rgb[..., 2] = (b1*255 + b2*255*((0.5*65535.0-a)*4)/65535.0 )
     return rgb
+
 
 def rgb_to_png(rgb, filename):
     im = Image.fromarray(rgb, "RGB")
--- cptv/reader.py
+++ cptv/reader.py
@@ -1,3 +1,17 @@
+# Copyright 2017 The Cacophony Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 import datetime
 import gzip
 
@@ -39,7 +53,7 @@ epoch = datetime.datetime(1970, 1, 1, tzinfo=datetime.timezone.utc)
 class CPTVReader:
     """
     CPTVReader is a parser and decompressor for Cacophony Project
-    Thermal Video files.
+    Thermal Video (CPTV) files.
 
     Usage:
 
