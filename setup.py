0d264a0fea46c7c9601bd4913eee92b3ee326ed4
Menno Finlay-Smits <inbox@menno.io>
2017-10-30 11:32:02 +1300

    Use ReST for the package description.

--- setup.py
+++ setup.py
@@ -1,14 +1,35 @@
 from setuptools import setup
-from pathlib import Path
 
 
-here = Path(__file__).parent
-with open(str(here / 'README.md'), encoding='utf-8') as f:
-    long_description = f.read()
+long_description = """\
+This is Python package provides for quick, easy parsing for Cacophony
+Project Thermal Video (CPTV) files. It works with Python 3 only.
+
+For more details on the internals of CPTV files, see the
+`specification`_.
+
+Example usage::
+
+    from cptv import CPTVReader
+
+
+    with open(filename, "rb") as f:
+        reader = CPTVReader(f)
+        print(reader.timestamp)
+        print(reader.x_resolution)
+        print(reader.y_resolution)
+
+        for frame in reader:
+            # Do something with frame.
+            # Each frame is a 2D numpy array.
+
+.. _`specification`: https://github.com/TheCacophonyProject/go-cptv/blob/master/SPEC.md
+"""
+
 
 setup(
     name='cptv',
-    version='0.1.0',
+    version='0.1.1',
 
     description='Python library for handling Cacophony Project Thermal Video (CPTV) files',
     long_description=long_description,
