1c5dd6f22f9d1a5cc792f88b72018fadfaafdfed
Menno Finlay-Smits <inbox@menno.io>
2018-07-19 11:09:10 +1200

    Fix source based deployments (Cython issues)

--- setup.py
+++ setup.py
@@ -1,5 +1,4 @@
-from setuptools import setup, find_packages
-from setuptools.extension import Extension
+from setuptools import setup, find_packages, Extension
 
 long_description = """\
 This is Python package provides for quick, easy parsing for Cacophony
@@ -26,47 +25,30 @@ Example usage::
 .. _`specification`: https://github.com/TheCacophonyProject/go-cptv/blob/master/SPEC.md
 """
 
-extensions = [
-    Extension("cptv.bitstream", ["cptv/bitstream.pyx"]),
-    Extension("cptv.reader", ["cptv/reader.pyx"]),
-]
-
-try:
-    from Cython.Build import cythonize
-    extensions = cythonize(extensions)
-except ImportError:
-    pass
-
-
 setup(
-    name='cptv',
-    version='0.2.0',
-
-    description='Python library for handling Cacophony Project Thermal Video (CPTV) files',
+    name="cptv",
+    version="0.2.0",
+    description="Python library for handling Cacophony Project Thermal Video (CPTV) files",
     long_description=long_description,
-    url='https://github.com/TheCacophonyProject/python-cptv',
-
-    author='The Cacophony Project',
-    author_email='dev@cacophony.org.nz',
-    license='Apache License 2.0',
-
+    url="https://github.com/TheCacophonyProject/python-cptv",
+    author="The Cacophony Project",
+    author_email="dev@cacophony.org.nz",
+    license="Apache License 2.0",
     # See https://pypi.python.org/pypi?%3Aaction=list_classifiers
     classifiers=[
-        'Development Status :: 4 - Beta',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: Apache Software License',
-        'Programming Language :: Python :: 3',
-        'Topic :: Software Development :: Libraries :: Python Modules',
-        'Topic :: Multimedia :: Video :: Conversion',
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: Apache Software License",
+        "Programming Language :: Python :: 3",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Topic :: Multimedia :: Video :: Conversion",
     ],
-
-    keywords='video compression',
-
-    packages=['cptv'],
-    ext_modules = extensions,
-
-    install_requires=['numpy', 'Pillow', 'opencv-python'],
-    extras_require={
-        'dev': ['Cython', 'pytest'],
-    },
+    keywords="video compression",
+    packages=["cptv"],
+    setup_requires=["setuptools>=18.0", "cython"],
+    ext_modules=[
+        Extension("cptv.reader", ["cptv/reader.pyx"]),
+        Extension("cptv.bitstream", ["cptv/bitstream.pyx"]),
+    ],
+    install_requires=["numpy", "Pillow", "opencv-python"],
 )
