0d4482dcdd4672829e7ded03d18b20ba7c89644b
Menno Finlay-Smits <inbox@menno.io>
2020-03-17 23:41:12 +1300

    Log when unknown field is encountered when reading

--- cptv/reader.py
+++ cptv/reader.py
@@ -14,36 +14,39 @@
 
 from datetime import datetime, timedelta, timezone
 import gzip
+import logging
 import struct
 import numpy as np
 
 from .frame import Frame
 
+logger = logging.getLogger("cptv")
+
 
 class Section:
-    HEADER = b'H'
-    FRAME = b'F'
+    HEADER = b"H"
+    FRAME = b"F"
 
 
 class Field:
     # Header fields
-    TIMESTAMP = b'T'
-    X_RESOLUTION = b'X'
-    Y_RESOLUTION = b'Y'
-    COMPRESSION = b'C'
-    DEVICENAME = b'D'
-    DEVICEID = b'I'
+    TIMESTAMP = b"T"
+    X_RESOLUTION = b"X"
+    Y_RESOLUTION = b"Y"
+    COMPRESSION = b"C"
+    DEVICENAME = b"D"
+    DEVICEID = b"I"
 
-    PREVIEW_SECS = b'P'
-    MOTION_CONFIG = b'M'
-    LATITUDE = b'L'
-    LONGITUDE = b'O'
+    PREVIEW_SECS = b"P"
+    MOTION_CONFIG = b"M"
+    LATITUDE = b"L"
+    LONGITUDE = b"O"
 
     # Frame fields
-    BIT_WIDTH = b'w'
-    FRAME_SIZE = b'f'
-    TIME_ON = b't'
-    LAST_FFC_TIME = b'c'
+    BIT_WIDTH = b"w"
+    FRAME_SIZE = b"f"
+    TIME_ON = b"t"
+    LAST_FFC_TIME = b"c"
 
 
 UINT32_FIELDS = {
@@ -55,21 +58,11 @@ UINT32_FIELDS = {
     Field.DEVICEID,
 }
 
-UINT8_FIELDS = {
-    Field.COMPRESSION,
-    Field.BIT_WIDTH,
-    Field.PREVIEW_SECS,
-}
+UINT8_FIELDS = {Field.COMPRESSION, Field.BIT_WIDTH, Field.PREVIEW_SECS}
 
-STRING_FIELDS = {
-    Field.DEVICENAME,
-    Field.MOTION_CONFIG
-}
+STRING_FIELDS = {Field.DEVICENAME, Field.MOTION_CONFIG}
 
-FLOAT_FIELDS = {
-    Field.LATITUDE,
-    Field.LONGITUDE
-}
+FLOAT_FIELDS = {Field.LATITUDE, Field.LONGITUDE}
 
 
 epoch = datetime(1970, 1, 1, tzinfo=timezone.utc)
@@ -127,8 +120,9 @@ class CPTVReader:
 
         self.compression = fields[Field.COMPRESSION]
         if self.compression != 1:
-            raise ValueError("unsupported compression type: {}"
-                             .format(self.compression))
+            raise ValueError(
+                "unsupported compression type: {}".format(self.compression)
+            )
 
         self.timestamp = fields[Field.TIMESTAMP]
         self.x_resolution = fields[Field.X_RESOLUTION]
@@ -163,17 +157,18 @@ class CPTVReader:
             workaround_bug_in_numpy_reading_from_gzip_files = True
             if workaround_bug_in_numpy_reading_from_gzip_files:
                 packed_delta = np.frombuffer(
-                    self.s.read(frame_size), dtype=np.dtype('B'))
+                    self.s.read(frame_size), dtype=np.dtype("B")
+                )
             else:
-                packed_delta = np.fromfile(self.s, np.dtype('B'), frame_size)
+                packed_delta = np.fromfile(self.s, np.dtype("B"), frame_size)
 
             pix = self._decompress_frame(linear_pix, packed_delta, bit_width)
 
             if self.version >= 2:
                 time_on = timedelta(milliseconds=fields.get(Field.TIME_ON, 0))
                 last_ffc_time = timedelta(
-                    milliseconds=fields.get(
-                        Field.LAST_FFC_TIME, 0))
+                    milliseconds=fields.get(Field.LAST_FFC_TIME, 0)
+                )
             else:
                 time_on = None
                 last_ffc_time = None
@@ -182,7 +177,7 @@ class CPTVReader:
 
     def _read_section(self, s):
         section_type = s.read(1)
-        if section_type == b'':
+        if section_type == b"":
             raise EOFError("short read")
         field_count = s.read(1)[0]
         fields = {}
@@ -212,6 +207,7 @@ class CPTVReader:
                 val = epoch
         else:
             # Unknown field, just slurp up the bytes
+            logger.warn("unknown field: %s (skipping)", ftype)
             val = s.read(data_len)
 
         return ftype, val
@@ -220,58 +216,61 @@ class CPTVReader:
         return s.read(1)[0]
 
     def _read_uint16(self, s):
-        return struct.unpack('<H', s.read(2))[0]
+        return struct.unpack("<H", s.read(2))[0]
 
     def _read_int32(self, s):
-        return struct.unpack('<i', s.read(4))[0]
+        return struct.unpack("<i", s.read(4))[0]
 
     def _read_uint32(self, s):
-        return struct.unpack('<I', s.read(4))[0]
+        return struct.unpack("<I", s.read(4))[0]
 
     def _read_uint64(self, s):
-        return struct.unpack('<Q', s.read(8))[0]
+        return struct.unpack("<Q", s.read(8))[0]
 
     def _read_float32(self, s):
-        return struct.unpack('<f', s.read(4))[0]
+        return struct.unpack("<f", s.read(4))[0]
 
     def _read_string(self, s, length):
         return s.read(length)
 
     def _decompress_frame(self, current_frame, source, packed_bit_width):
-        s = np.empty(self.x_resolution * self.y_resolution, dtype='h')
-        s[0] = struct.unpack('<i', source[0:4])[0]  # starting value, signed
+        s = np.empty(self.x_resolution * self.y_resolution, dtype="h")
+        s[0] = struct.unpack("<i", source[0:4])[0]  # starting value, signed
 
         if packed_bit_width > 16:
-            raise IOError('Higher than 16bit thermal imaging not supported')
+            raise IOError("Higher than 16bit thermal imaging not supported")
 
         if packed_bit_width == 8:
-            s[1:] = source[4:].astype('b')
+            s[1:] = source[4:].astype("b")
         else:
             source = np.append(source, np.zeros(1))  # protect against overrun
 
             (lookup_high, lookup_low, lookup_bit) = self._fetch_aux(packed_bit_width)
 
-            s[1:] = 256 * source[lookup_high] + \
-                source[lookup_low]  # fetch nearby 16 bits
+            s[1:] = (
+                256 * source[lookup_high] + source[lookup_low]
+            )  # fetch nearby 16 bits
 
             # twos complement and shift down into range
             mask = (1 << packed_bit_width) - 1
             max_packed_value = 1 << (packed_bit_width - 1)
-            s[1:] = (((s[1:] >> lookup_bit) + max_packed_value)
-                     & mask) - max_packed_value
+            s[1:] = (
+                ((s[1:] >> lookup_bit) + max_packed_value) & mask
+            ) - max_packed_value
         # TODO: Fast path for 12bit and 16bit
 
         current_frame += np.cumsum(s)  # expand deltas and delta-deltas
         pix_signed = current_frame[self._get_snake()]  # remove snake ordering
-        return pix_signed.astype('H')  # cast unsigned
+        return pix_signed.astype("H")  # cast unsigned
 
     def _get_snake(self):
-        if not hasattr(self, 'snake_cache'):
+        if not hasattr(self, "snake_cache"):
             width = self.x_resolution
             height = self.y_resolution
-            linear = np.arange(width * height, dtype='I')
-            twisted = linear + ((linear // width) & 1) * \
-                (width - 1 - 2 * (linear % width))
+            linear = np.arange(width * height, dtype="I")
+            twisted = linear + ((linear // width) & 1) * (
+                width - 1 - 2 * (linear % width)
+            )
             self.snake_cache = twisted.reshape(height, width)
         return self.snake_cache
 
@@ -283,9 +282,11 @@ class CPTVReader:
         key = (width, height, packed_bit_width)
         if not key in self.lookup_cache:
             lookup = np.arange(0, width * height - 1) * packed_bit_width
-            lookup_byte = lookup // 8 + 5  # 8 bits per byte, with 4+1 bytes offset from start
+            lookup_byte = (
+                lookup // 8 + 5
+            )  # 8 bits per byte, with 4+1 bytes offset from start
             lookup_bit = 16 - packed_bit_width - (lookup & 7)
             # 'I' might be faster on arm? need to profile
-            lookup_bit = lookup_bit.astype('B')
+            lookup_bit = lookup_bit.astype("B")
             self.lookup_cache[key] = (lookup_byte - 1, lookup_byte, lookup_bit)
         return self.lookup_cache[key]
